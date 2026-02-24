8a6dbf2a0a950c0551ea272f95314c0211cfd929
Cain Seing <20571728+cainseing@users.noreply.github.com>
2026-02-24 20:58:02 +1100

    release 0.3.0-beta

--- Formula/drop.rb
+++ Formula/drop.rb
@@ -1,29 +1,28 @@
 class Drop < Formula
   desc "Secure, zero-knowledge, secret sharing CLI"
   homepage "https://getdrop.dev"
-  version "0.2.1-beta" # Remember to bump this with your releases
+  version "0.3.0-beta"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://raw.githubusercontent.com/cainseing/drop-cli/v0.2.1-beta/bin/drop-darwin-arm64"
-      sha256 "0b4fa075bb27d8ad647eead3ef159d112362d56a6fe1156e30b3a1be1f1b5776"
+      url "https://raw.githubusercontent.com/cainseing/drop-cli/v0.3.0-beta/bin/drop-darwin-arm64"
+      sha256 "963c9fec6e9e550946d72ef2b51f16eb2309e375785642da17951b7c87358c43"
     else
-      url "https://raw.githubusercontent.com/cainseing/drop-cli/v0.2.1-beta/bin/drop-darwin-amd64"
-      sha256 "93489f4761c771b9dca0283213b14935803b90ee647b10ed84e24f947ee6720a"
+      url "https://raw.githubusercontent.com/cainseing/drop-cli/v0.3.0-beta/bin/drop-darwin-amd64"
+      sha256 "b032affb61ddf1143887a1b12ddb0b9d01d6ab7c05425e7b4d89370a1b19bbec"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://raw.githubusercontent.com/cainseing/drop-cli/v0.2.1-beta/bin/drop-linux-arm64"
-      sha256 "5c9734899f739d5c9a3666a1dfa437782e44b2ef9984f5e410105d3271b635aa"
+      url "https://raw.githubusercontent.com/cainseing/drop-cli/v0.3.0-beta/bin/drop-linux-arm64"
+      sha256 "593ad5b87616aeade8308a71e5fa84928523a3cf4d83e0b2af6e28c4b3f92758"
     else
-      url "https://raw.githubusercontent.com/cainseing/drop-cli/v0.2.1-beta/bin/drop-linux-amd64"
-      sha256 "9fa8e36c6a44b06045d07a9a310e2dfbdae14117f80879256711c0e43c70a1f0"
+      url "https://raw.githubusercontent.com/cainseing/drop-cli/v0.3.0-beta/bin/drop-linux-amd64"
+      sha256 "30d59cd26405089340b1310d999972516adae126a993a1055949a90721cd1220"
     end
   end
 
   def install
-    # Dynamically find the downloaded binary and install it as 'drop'
     binary_name = Dir["drop-*"].first
     
     if binary_name.nil?
